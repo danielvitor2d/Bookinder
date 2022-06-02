1deb54741dbabea19267aa0efe7e64f4c4beb491
danielvitor2d <ripsea.rpg321@gmail.com>
2022-06-01 23:59:34 -0300

    Create Sign In screen

---
+++ app/src/main/java/com/mobile/bookinder/screens/sign_in/SignIn.kt
@@ -0,0 +1,12 @@
+package com.mobile.bookinder.screens.sign_in
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mobile.bookinder.R
+
+class SignIn: AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.sign_in)
+  }
+}
