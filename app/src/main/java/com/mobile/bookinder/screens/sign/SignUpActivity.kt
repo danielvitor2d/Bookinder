313432ff1586bcff443aa90a18d892749b191684
GuimaraesSl <alsamir.gabriel@gmail.com>
2022-06-08 23:37:49 -0300

    SignIn screen implemented + SignUp created and linked to SignIn

---
+++ app/src/main/java/com/mobile/bookinder/screens/sign/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.mobile.bookinder.screens.sign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mobile.bookinder.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
