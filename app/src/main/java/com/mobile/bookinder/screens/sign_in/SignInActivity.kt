4073b710160d43c7bf1787e743c651c7bf796c11
danielvitor2d <ripsea.rpg321@gmail.com>
2022-06-12 17:37:19 -0300

    Update

--- app/src/main/java/com/mobile/bookinder/screens/sign_in/SignInActivity.kt
+++ app/src/main/java/com/mobile/bookinder/screens/sign_in/SignInActivity.kt
@@ -4,16 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.mobile.bookinder.common.Book
 import com.mobile.bookinder.common.User
 import com.mobile.bookinder.databinding.ActivitySignInBinding
 import com.mobile.bookinder.screens.home.Home
 import com.mobile.bookinder.screens.sign_up.SignUpActivity
+import java.util.*
 
 class SignInActivity : AppCompatActivity() {
 
   private val users: MutableList<User> = mutableListOf(
-    User("danielvitor.p1@gmail.com", "daniel123")
+    User(UUID.randomUUID(), "danielvitor.p1@gmail.com", "daniel123")
   )
+
+  private val books: MutableList<Book> = mutableListOf()
 
   private lateinit var binding: ActivitySignInBinding
 
@@ -41,8 +45,7 @@ class SignInActivity : AppCompatActivity() {
       var validCredentials: Boolean = false
 
       users.forEach { user ->
-//        Toast.makeText(this, "{$fieldEmail}:{${user.email}}", Toast.LENGTH_LONG).show()
-//        Toast.makeText(this, "{$fieldPassword}:{${user.password}}", Toast.LENGTH_LONG).show()
+//        Toast.makeText(this, "{${user.user_id}}, {${user.email}}, {${user.password}}", Toast.LENGTH_LONG).show()
         if (user.email == fieldEmail && user.password == fieldPassword) {
           validCredentials = true
           return@forEach
@@ -50,10 +53,10 @@ class SignInActivity : AppCompatActivity() {
       }
 
       if (validCredentials) {
-        binding.editTextEmail.setText("")
-        binding.editTextPassword.setText("")
         val intent = Intent(this, Home::class.java)
         startActivity(intent)
+        binding.editTextEmail.setText("")
+        binding.editTextPassword.setText("")
       } else {
         Toast.makeText(this, "Email e/ou senha inválidos", Toast.LENGTH_SHORT).show()
       }
