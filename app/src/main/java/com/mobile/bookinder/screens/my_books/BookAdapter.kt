fa6c476a13d6d3d7e28206304a8b3116680ed4b7
Daniel Vitor <ripsea.rpg321@gmail.com>
2022-06-17 12:55:12 -0300

    Add action remove book

--- app/src/main/java/com/mobile/bookinder/screens/my_books/BookAdapter.kt
+++ app/src/main/java/com/mobile/bookinder/screens/my_books/BookAdapter.kt
@@ -3,11 +3,12 @@ package com.mobile.bookinder.screens.my_books
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.mobile.bookinder.R
 
-class BookAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class BookAdapter(private val clickListener: (String, Int) -> Unit): RecyclerView.Adapter<BookAdapter.MessageViewHolder>() {
   private val books: MutableList<String> =
     mutableListOf(
       "Livro: As aventuras de Al Samir",
@@ -26,25 +27,35 @@ class BookAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
       "Livro: O homem de giz",
       "Livro: Nárnia")
 
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+  public fun removeItem(position: Int) {
+    books.removeAt(position)
+    notifyItemRemoved(position)
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
     val card = LayoutInflater
       .from(parent.context)
       .inflate(R.layout.message_card_my_books, parent, false)
-    return MessageViewHolder(card)
+    return MessageViewHolder(card) {
+      clickListener(books[it], it)
+    }
   }
 
-  override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-    val currentItem = books[position]
-    if (holder is MessageViewHolder) {
-      holder.bookMessage.text = currentItem
-    }
+  override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+    holder.bookMessage.text = books[position]
   }
 
   override fun getItemCount(): Int {
     return books.size
   }
 
-  class MessageViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+  class MessageViewHolder(itemView: View, clickAtPosition: (Int) -> Unit): RecyclerView.ViewHolder(itemView) {
     val bookMessage: TextView = itemView.findViewById(R.id.likeMessage)
+
+    init {
+      itemView.findViewById<ImageButton>(R.id.imageButtonRemoveBook).setOnClickListener {
+        clickAtPosition(adapterPosition)
+      }
+    }
   }
 }
--- app/src/main/java/com/mobile/bookinder/screens/my_books/MyBooksFragment.kt
+++ app/src/main/java/com/mobile/bookinder/screens/my_books/MyBooksFragment.kt
@@ -1,9 +1,12 @@
 package com.mobile.bookinder.screens.my_books
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,17 +21,33 @@ class MyBooksFragment: Fragment() {
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
-  ): View? {
+  ): View {
     _binding = FragmentMyBooksBinding.inflate(inflater, container, false)
     setUpRecyclerView(binding.root)
-
     return binding.root
   }
 
   private fun setUpRecyclerView(view: View) {
     val itemList = view.findViewById<RecyclerView>(R.id.itemListMyBooks)
     itemList.layoutManager = LinearLayoutManager(view.context)
-    itemList.adapter = BookAdapter()
+    itemList.adapter = BookAdapter() { book, pos ->
+      val adapter = itemList.adapter
+      if (adapter !is BookAdapter) return@BookAdapter
+      actionRemoveBook(binding.root.context, adapter, book, pos)
+    }
+  }
+
+  private fun actionRemoveBook(context: Context, bookAdapter: BookAdapter, book: String, pos: Int) {
+    val alertDialogBuilder = AlertDialog.Builder(context)
+    alertDialogBuilder.setTitle("Deseja remover o livro $book?")
+    alertDialogBuilder.setPositiveButton("Sim") { dialog, _ ->
+      bookAdapter.removeItem(pos)
+      dialog.dismiss()
+    }
+    alertDialogBuilder.setNegativeButton("Não") { dialog, _ ->
+      dialog.dismiss()
+    }
+    alertDialogBuilder.show()
   }
 
   override fun onDestroyView(){
